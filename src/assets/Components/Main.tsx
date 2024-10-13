da9c34a3414b44f870917b223c29b2f9bdf1c657
coyote4682 <jasphergargar@gmail.com>
2024-10-13 20:01:52 +0800

    tech stacks logo added

--- src/assets/Components/Main.tsx
+++ src/assets/Components/Main.tsx
@@ -1,4 +1,8 @@
 import { Instagram, Facebook, Linkedin } from 'lucide-react';
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiChakraui } from "react-icons/si";
+import { SiMui } from "react-icons/si";
 
 
 const Main = () => {
@@ -30,24 +34,29 @@ const Main = () => {
        </div>   
         </a>
       </div>
-
       <div className="mt-24 text-md">
-        <a href="#" className="flex flex-col items-center bg-gray-800 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl">
-        <img 
-            className="rounded-full border-2 border-sky-600 my-10 h-52 md:h-auto md:w-48 md:rounded-full md:rounded-s-lg" 
-            src="/images/profile.jpg" 
-            alt="Profile"
-        />
+      <a href="#" className="flex flex-col items-center bg-gray-800 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl">
         <div className="flex flex-col justify-between p-4 tracking-normal md:p-6">
-            <h5 className="text-md text-left font-semibold text-white">
-                Hi! I'm Jaspher ✌
-            </h5>
-            <p className="my-8 mb-2 mx-2 text-sm text-left leading-relaxed text-gray-200">
-                I am a dedicated Front-End Developer with a strong passion for building intuitive and engaging web applications. With a solid foundation in HTML, CSS, and JavaScript, I specialize in creating responsive designs that enhance user experiences across various devices.
-            </p>
-       </div>   
-        </a>
-      </div>
+          <h5 className="text-md text-left font-semibold text-white">Tech Stacks</h5>
+          <ul>
+          <li className="h-20">
+            <FaReact className="text-blue-500" size={40} />
+          </li>
+          <li>
+            <RiTailwindCssFill className="text-sky-500"size={40}/>
+          </li>
+          <li>
+            <SiChakraui className="text-teal-600"size={40}/>
+          </li>
+          <li>
+            <SiMui className="text-blue-500"size={40}/></li>
+          </ul>
+          <p className="my-8 mb-2 mx-2 text-sm text-left leading-relaxed text-gray-200">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, quod labore. Quisquam iusto vitae quos aliquid eveniet, quia est sapiente natus quas reiciendis, veniam maiores corrupti assumenda impedit fugit hic?
+          </p>
+        </div>
+      </a>
+    </div>
       
       </div>
     );  
