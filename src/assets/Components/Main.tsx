df157ebfafe7c1103ab666c568854912cadf5f8a
coyote4682 <jasphergargar@gmail.com>
2024-10-27 13:51:37 +0800

    new icons

--- src/assets/Components/Main.tsx
+++ src/assets/Components/Main.tsx
@@ -1,25 +1,32 @@
 
-import { FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Main = () => {
     return(
       <main className="px-12 lg:px-[200px] mt-[140px]">
         <div className="lg:text-center">
           <h2 className="text-6xl lg:text-[80px] mt-9 text-slate-300 font-inter font-bold tracking-tighter">Hi, I'm <span className="text-indigo-600">Jaspher Gargar</span></h2>
-          <p  className="mt-5 mb-4 text-xl text-slate-100 font-inter font-bold lg:text-[28px] lg:leading-relaxed lg:tracking-tight uppercase">Frontend Developer</p>
+          <p  className="mt-5 mb-4 text-xl text-slate-300 font-inter font-bold lg:text-[28px] lg:leading-relaxed lg:tracking-tight uppercase">Frontend Developer</p>
         </div>
          
       <div className="text-3xl  flex justify-center space-x-8 py-6">
-        <a href="https://www.facebook.com/jasp.gar2/" target="_blank" rel="">
-         <FaFacebook className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-indigo-600"/>
-        </a>
         
         <a href="https://www.instagram.com/psaj__/" target="_blank" rel="">
         <FaInstagram className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-indigo-600"  />
         </a>
-        
+  
         <a href="" target="_blank" rel="noopener noreferrer">
-        <FaLinkedin className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-indigo-600"/>
+        <FiGithub className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-indigo-600"/>
+        </a>
+
+        <a href="" target="_blank" rel="noopener noreferrer">
+        <FiLinkedin className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-indigo-600"/>
+        </a>
+
+        <a href="" target="_blank" rel="noopener noreferrer">
+        <FaXTwitter className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-indigo-600"/>
         </a>
         <div>
           
