3996d8882a89f0fd55157510b06779fa47f4351a
coyote4682 <jasphergargar@gmail.com>
2024-10-29 22:31:05 +0800

    changed Main layout and transitions

--- src/assets/Components/Main.tsx
+++ src/assets/Components/Main.tsx
@@ -2,28 +2,34 @@
 import { FaInstagram } from "react-icons/fa";
 import { FiGithub, FiLinkedin } from "react-icons/fi";
 import { FaXTwitter } from "react-icons/fa6";
+import { IoMdPulse } from "react-icons/io"
 import * as motion from "framer-motion/client"
 
 
 const Main = () => {
     return(
-      <main className="px-12 mt-[140px] lg:px-[200px] lg:mt-[160px] ">
+      <main className="px-12 mt-[140px] lg:px-[200px] lg:mt-[120px] ">
         <div className="lg:text-center">
+          <motion.h1
+             initial={{ y: -20, opacity: 0 }} // Start position (above) and opacity
+             animate={{ y: 0, opacity: 1 }} // End position and opacity
+             transition={{ duration: 0.5, delay: 1.5 }} // Animation timing
+          className="flex justify-center text-2xl text-indigo-600"><IoMdPulse /></motion.h1>
           <motion.h2 initial={{x: -100, opacity: 0}}
           animate= {{x:0, opacity: 1}}
-          transition={{duration: 0.5, delay: 0.5}} className="text-4xl lg:text-[80px] mt-9 text-slate-300 font-inter font-bold tracking-tighter">Hi, I'm <span className="text-indigo-600">Jaspher Gargar</span></motion.h2>
+          transition={{duration: 0.5, delay: 0.5}} className="text-4xl lg:text-[75px] mt-9 text-slate-300 font-inter font-bold tracking-tight">Crafting Digital <span className="">Magic</span></motion.h2>
           <motion.p 
           initial={{x: -100, opacity: 0}}
           animate= {{x:0, opacity: 1}}
           transition={{duration: 0.5, delay: 0.8}}
-          className="mt-5 mb-4 text-xl text-slate-300 font-inter font-bold leading-relaxed tracking-tight lg:text-[40px] lg:mt-10 ">Frontend Developer</motion.p>
+          className="mt-4 mb-4 text-xl text-indigo-600 font-inter font-bold leading-relaxed tracking-tight lg:text-[23px] lg:mt-12 "> Jaspher Gargar <span className="text-slate-400">| Frontend Developer</span></motion.p>
         </div>
          
       <motion.div 
        initial={{ y: 100, opacity: 0 }} 
        animate={{ y: 0, opacity: 1 }}  
        transition={{ duration: 0.5, delay: 1 }}  
-       className="text-start text-xl flex space-x-8 py-6 lg:text-3xl lg:justify-center">
+       className="text-start text-xl flex space-x-8 py-6 lg:text-2xl lg:justify-center">
         
         <a href="https://www.instagram.com/psaj__/" target="_blank" rel="">
         <FaInstagram className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-indigo-600"  />
