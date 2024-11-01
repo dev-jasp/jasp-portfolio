ab297068810123ddb7d5f4660ab7391cccb12421
coyote4682 <jasphergargar@gmail.com>
2024-11-01 11:56:53 +0800

    text-color changed to cyan-600

--- src/assets/Components/Sidebar.tsx
+++ src/assets/Components/Sidebar.tsx
@@ -24,13 +24,13 @@ const Sidebar = () => {
                     initial={{x: -100, opacity: 0}}
                     animate= {{x:0, opacity: 1}}
                     transition={{duration: 0.5, delay: 0.9}}
-                    className="text-indigo-700 text-xl mb-4 font-bold tracking-tight">Frontend Developer</motion.p>
+                    className="text-cyan-600 text-xl mb-4 font-bold tracking-tight">Frontend Developer</motion.p>
                 <motion.p 
                     initial={{x: -100, opacity: 0}}
                     animate= {{x:0, opacity: 1}}
                     transition={{duration: 0.5, delay: 1.1}}
                     className="flex flex-row text-gray-400 mb-5">
-                    <MdLocationOn className="m-1 text-lg text-indigo-600" />
+                    <MdLocationOn className="mt-1 text-lg text-cyan-600" />
                 <span className="ml-1 tracking-tight">Zamboanga City, Philippines</span>
                 </motion.p>
                 <motion.p 
@@ -38,7 +38,7 @@ const Sidebar = () => {
                     animate= {{x:0, opacity: 1}}
                     transition={{duration: 0.5, delay: 1.3}}
                     className="text-gray-300 text-sm mb-4 "> Crafting Tech-infused Creativity  
-                    <span className="text-indigo-700 text-base font-extrabold"> |</span> Evolving with Technology
+                    <span className="text-cyan-600 text-base font-extrabold"> |</span> Evolving with Technology
                 </motion.p>
             </div>
            
@@ -48,24 +48,24 @@ const Sidebar = () => {
                     animate={{ y: 0, opacity: 1 }}
                     transition={{ duration: 0.5, delay: 1.4 }}
                     className="text-base mr-2">
-                    <button className="flex items-center mt-2 space-x-2 px-2 py-1 bg-transparent border-indigo-700 border-2 text-gray-300 rounded-md hover:bg-indigo-700 hover:text-white transition">
-                        <span className="tracking-normal">Checkout My CV</span>
+                    <button className="flex items-center mt-2 space-x-2 px-2 py-1 bg-transparent border-cyan-600 border-2 text-gray-300 rounded-md hover:bg-cyan-600 hover:text-white transition">
+                        <span className="tracking-normal">Dowload My CV</span>
                         <MdDownload className="text-xl mt-1"/>
                     </button>
                 </motion.div>
                 <Animation1>
                 <div className="flex space-x-4">
                     <a href="https://www.instagram.com/psaj__/" target="_blank" rel="noopener noreferrer">
-                        <FaInstagram className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-indigo-600 mt-2" />
+                        <FaInstagram className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-cyan-600 mt-2" />
                     </a>
                     <a href="https://www.linkedin.com/in/your-profile/" target="_blank" rel="noopener noreferrer">
-                        <FiLinkedin className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-indigo-600 mt-2" />
+                        <FiLinkedin className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-cyan-600 mt-2" />
                     </a>
                     <a href="https://twitter.com/your-profile" target="_blank" rel="noopener noreferrer">
-                        <FaXTwitter className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-indigo-600 mt-2" />
+                        <FaXTwitter className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-cyan-600 mt-2" />
                     </a>
                     <a href="https://github.com/nattycodes2" target="_blank" rel="noopener noreferrer">
-                        <FiGithub className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-indigo-600 mt-2"/>
+                        <FiGithub className="text-gray-500 transform transition-transform ease-in-out duration-200 hover:scale-110 hover:text-cyan-600 mt-2"/>
                     </a>
                     </div>
                 </Animation1>
