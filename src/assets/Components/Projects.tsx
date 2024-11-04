830890cb7fc2dffea82be96c550479c576c205ba
coyote4682 <jasphergargar@gmail.com>
2024-11-04 21:24:15 +0800

    fixed the padding of <Project/> in lg screen

--- src/assets/Components/Projects.tsx
+++ src/assets/Components/Projects.tsx
@@ -3,8 +3,8 @@ function Projects() {
 
   return (
     <section>
-        <div className="px-6 lg:ml-[600px] bg-gradient-to-r from-slate-900 to-gray-900 p-8 h-screen font-sans">
-            <h1 className="text-cyan-600 text-3xl lg:text-3xl font-bold mb-10">Projects</h1>
+        <div className="px-6 lg:ml-[600px] bg-gradient-to-r from-slate-900 to-gray-900 h-screen font-sans">
+            <h1 className="text-cyan-600 text-3xl lg:text-3xl font-bold mb-10 lg:ml-4">Projects</h1>
         </div>
     </section>
   )
