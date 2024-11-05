248da7095e1da20ae93aeca994b3323db4ca9ba2
coyote4682 <jasphergargar@gmail.com>
2024-11-05 22:34:41 +0800

    footer component added

--- src/assets/Components/Footer.tsx
+++ src/assets/Components/Footer.tsx
@@ -1,7 +1,12 @@
 
+
 function Footer() {
   return (
-    <div>Footer</div>
+    <section>
+    <div className="px-6 lg:ml-[600px] bg-gradient-to-r from-slate-900 to-gray-900 h-screen font-sans">
+        <h1 className="text-cyan-600 text-3xl lg:text-3xl font-bold mb-10 lg:ml-4">Footer</h1>
+    </div>
+</section>
   )
 }
 
