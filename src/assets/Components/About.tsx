3d76b1ae15b2d2dc862d25450f59c2ecf6e979f2
coyote4682 <jasphergargar@gmail.com>
2024-11-07 20:59:19 +0800

    added animation for icons

--- src/assets/Components/About.tsx
+++ src/assets/Components/About.tsx
@@ -3,6 +3,15 @@ import { SiTypescript } from "react-icons/si";
 import { RiReactjsFill, RiTailwindCssFill, RiNextjsFill } from "react-icons/ri";
 import { SiMui, SiChakraui } from "react-icons/si";
 import { TbBrandFramerMotion } from "react-icons/tb";
+import { motion } from "framer-motion"; 
+
+
+const   iconAnimation = {
+  hidden: {
+    opacity: 0, 
+
+  }
+}
 
 function About() {
   return (
@@ -31,37 +40,62 @@ function About() {
         </div>
       <div className="flex flex-wrap mb-4 ">
       <div className="flex flex-col items-center mr-4 mb-4">
-        <RiReactjsFill className="h-14 w-16 text-2xl text-react mb-3" />
+        <motion.span
+        whileHover={{scale: 1.3}}
+        transition={{type: 'spring', stiffness: 300, damping: 8}}
+        >
+          <RiReactjsFill className="h-14 w-16 text-2xl text-react mb-3" />
+        </motion.span>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
           ReactJS
         </span>
       </div>
       <div className="flex flex-col items-center mr-4 mb-4">
-        <RiNextjsFill className="h-14 w-16 text-2xl text-slate-200 mb-3" />
+        <motion.span
+        whileHover={{scale: 1.3}}
+        transition={{type: 'spring', stiffness: 300, damping: 8}}
+        >
+          <RiNextjsFill className="h-14 w-16 text-2xl text-slate-200 mb-3" /></motion.span>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
           NextJS
         </span>
       </div>
       <div className="flex flex-col items-center mr-4 mb-4">
-        <RiTailwindCssFill className="h-14 w-16 text-2xl text-tailwind mb-3" />
+        <motion.span
+        whileHover={{scale: 1.3}}
+        transition={{type: 'spring', stiffness: 300, damping: 8}}
+        >
+          <RiTailwindCssFill className="h-14 w-16 text-2xl text-tailwind mb-3" /></motion.span>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
           Tailwindcss
         </span>
       </div>
       <div className="flex flex-col items-center mr-4 mb-4">
-        <SiChakraui className="h-14 w-12 text-2xl text-chakra mb-3" />
+        <motion.span
+        whileHover={{scale: 1.3}}
+        transition={{type: 'spring', stiffness: 300, damping: 8}}
+        >
+          <SiChakraui className="h-14 w-12 text-2xl text-chakra mb-3" /></motion.span>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
           Chakra UI
         </span>
       </div>
       <div className="flex flex-col items-center mr-4 mb-4">
-        <SiMui className="h-14 w-12 text-2xl text-mui mb-3" />
+        <motion.span
+        whileHover={{scale: 1.3}}
+        transition={{type: 'spring', stiffness: 300, damping: 8}}
+        >
+          <SiMui className="h-14 w-12 text-2xl text-mui mb-3" /></motion.span>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
           Material UI
         </span>
       </div>
       <div className="flex flex-col items-center mr-4 mb-4">
-        <TbBrandFramerMotion className="h-14 w-16 text-2xl text-framerMotion mb-3" />
+        <motion.span
+        whileHover={{scale: 1.3}}
+        transition={{type: 'spring', stiffness: 300, damping: 10}}
+        >
+          <TbBrandFramerMotion className="h-14 w-16 text-2xl text-framerMotion mb-3" /></motion.span>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
           Framer Motion
         </span>
