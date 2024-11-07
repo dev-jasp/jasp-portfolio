ced3994d39ce5d08b782e08ab5346591540a7797
coyote4682 <jasphergargar@gmail.com>
2024-11-07 22:33:04 +0800

    antDesign ui installed

--- src/assets/Components/About.tsx
+++ src/assets/Components/About.tsx
@@ -4,14 +4,8 @@ import { RiReactjsFill, RiTailwindCssFill, RiNextjsFill } from "react-icons/ri";
 import { SiMui, SiChakraui } from "react-icons/si";
 import { TbBrandFramerMotion } from "react-icons/tb";
 import { motion } from "framer-motion"; 
+import CustomTooltip from './Tooltip/CustomTooltip'; 
 
-
-const   iconAnimation = {
-  hidden: {
-    opacity: 0, 
-
-  }
-}
 
 function About() {
   return (
@@ -23,16 +17,26 @@ function About() {
         </div>
       <div className="flex flex-wrap mb-4">
       <div className="flex flex-col items-center mr-4 mb-4">
-        <DiJavascript className="h-14 w-16 text-2xl text-javascript mb-3" />
+        <motion.span
+        whileHover={{scale: 1.2}}
+        transition={{type: 'spring', stiffness: 300, damping: 10}}
+        >
+          <DiJavascript className="h-14 w-16 text-2xl text-javascript mb-3" /></motion.span>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
           JavaScript
         </span>
       </div>
       <div className="flex flex-col items-center mr-4 mb-4">
-        <SiTypescript className="h-14 w-11 text-2xl text-typescript mb-3" />
+        <motion.span
+        whileHover={{scale: 1.2}}
+        transition={{type: 'spring', stiffness: 300, damping: 10}}
+        >
+          <SiTypescript className="h-14 w-11 text-2xl text-typescript mb-3" /></motion.span>
+        <CustomTooltip title='Holabels'>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
           TypeScript
         </span>
+        </CustomTooltip>
       </div>
       </div>
        <div>
@@ -41,19 +45,21 @@ function About() {
       <div className="flex flex-wrap mb-4 ">
       <div className="flex flex-col items-center mr-4 mb-4">
         <motion.span
-        whileHover={{scale: 1.3}}
-        transition={{type: 'spring', stiffness: 300, damping: 8}}
+        whileHover={{scale: 1.2}}
+        transition={{type: 'spring', stiffness: 300, damping: 10}}
         >
           <RiReactjsFill className="h-14 w-16 text-2xl text-react mb-3" />
         </motion.span>
-        <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
-          ReactJS
-        </span>
+        <CustomTooltip title="TypeScript">
+              <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
+                TypeScript
+              </span>
+            </CustomTooltip>
       </div>
       <div className="flex flex-col items-center mr-4 mb-4">
         <motion.span
-        whileHover={{scale: 1.3}}
-        transition={{type: 'spring', stiffness: 300, damping: 8}}
+        whileHover={{scale: 1.2}}
+        transition={{type: 'spring', stiffness: 300, damping: 10}}
         >
           <RiNextjsFill className="h-14 w-16 text-2xl text-slate-200 mb-3" /></motion.span>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
@@ -62,8 +68,8 @@ function About() {
       </div>
       <div className="flex flex-col items-center mr-4 mb-4">
         <motion.span
-        whileHover={{scale: 1.3}}
-        transition={{type: 'spring', stiffness: 300, damping: 8}}
+        whileHover={{scale: 1.2}}
+        transition={{type: 'spring', stiffness: 300, damping: 10}}
         >
           <RiTailwindCssFill className="h-14 w-16 text-2xl text-tailwind mb-3" /></motion.span>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
@@ -72,8 +78,8 @@ function About() {
       </div>
       <div className="flex flex-col items-center mr-4 mb-4">
         <motion.span
-        whileHover={{scale: 1.3}}
-        transition={{type: 'spring', stiffness: 300, damping: 8}}
+        whileHover={{scale: 1.2}}
+        transition={{type: 'spring', stiffness: 300, damping: 10}}
         >
           <SiChakraui className="h-14 w-12 text-2xl text-chakra mb-3" /></motion.span>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
@@ -82,8 +88,8 @@ function About() {
       </div>
       <div className="flex flex-col items-center mr-4 mb-4">
         <motion.span
-        whileHover={{scale: 1.3}}
-        transition={{type: 'spring', stiffness: 300, damping: 8}}
+        whileHover={{scale: 1.2}}
+        transition={{type: 'spring', stiffness: 300, damping: 10}}
         >
           <SiMui className="h-14 w-12 text-2xl text-mui mb-3" /></motion.span>
         <span className="text-cyan-500 px-3 py-1.5 text-xs bg-cyan-500 bg-opacity-20 font-medium rounded-full cursor-pointer">
@@ -92,7 +98,7 @@ function About() {
       </div>
       <div className="flex flex-col items-center mr-4 mb-4">
         <motion.span
-        whileHover={{scale: 1.3}}
+        whileHover={{scale: 1.2}}
         transition={{type: 'spring', stiffness: 300, damping: 10}}
         >
           <TbBrandFramerMotion className="h-14 w-16 text-2xl text-framerMotion mb-3" /></motion.span>
