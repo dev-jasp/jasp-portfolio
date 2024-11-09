91064fd8e3ab51e61e10c65e32772c5fa6489226
coyote4682 <jasphergargar@gmail.com>
2024-11-09 13:09:28 +0800

    footer section hidden in lg screen

--- src/assets/Components/Footer.tsx
+++ src/assets/Components/Footer.tsx
@@ -3,7 +3,7 @@
 function Footer() {
   return (
     <section>
-    <div className="py-12 px-6 w-full lg:ml-[600px] lg:px-10 lg:items-center xl:ml-[40%] xl:px-16 lg:bg-gradient-to-r  from-slate-950 to-gray-900">
+    <div className="py-12 px-6 w-full lg:hidden xl:hidden lg:ml-[600px] lg:px-10 lg:items-center xl:ml-[40%] xl:px-16 lg:bg-gradient-to-r  from-slate-950 to-gray-900">
         <h1 className="text-cyan-600 text-3xl lg:text-3xl font-bold mb-12">Footer</h1>
         <button className="text-slate-200 text-xl lg:text-2xl font-bold px-4 py-2 border-2 bg-transparent rounded-full hover:bg-cyan-500 hover:border-cyan-500 ">
           <a href="https://www.linkedin.com/?original_referer=">Contact Me!</a>
